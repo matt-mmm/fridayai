edefb60592ee7446d9e3b5f001d50938a9c7c615
Matthew Manganillo <matthewmanganillo@dyn210211.cc.lehigh.edu>
2024-04-21 19:59:19 -0400

    added api integration and initialized speech recognition

--- friday.py
+++ friday.py
@@ -3,4 +3,11 @@ import speech_recognition as sr
 from gtts import gTTS
 import os
 from openai import OpenAI
+import playsound
+
+#insert your api key here!!!
+client = OpenAI(api_key='your api key here')
+
+#initializing speech recognition recognizer
+r = sr.Recognizer()
 
